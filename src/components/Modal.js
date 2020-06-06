fcf2b5cd06d1c5a1163e1ff74a2d97e8f82fe403
Helio Cardoso <cardoso_helio@hotmail.com.(none)>
2020-06-06 18:43:49 +0100

    add redux

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,34 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
+import { isEmpty } from 'lodash';
 
-const Modal = ({ handleClose, show, showRobot }) => {
-    const showHideClassName = show ? "modal display-block" : "modal display-none";
-    let robotInfo = showRobot || {};
-    const {
-        name, username, email, website, phone,
-        address: {city: addressCity} = {addressCity: 'No available city'},
-        address: {zipcode} = {zipcode: 'No zipcode available'},
-        company: {catchPhrase} = {catchPhrase: 'No catch phrase available'},
-        company: {name: companyName} = {companyName: 'No company name available'},
-    } = robotInfo;
+class Modal extends Component {
+    constructor() {
+        super();
+        this.state = {
+            modal14: false
+        }
+    }
 
-    return (
-        <div className={showHideClassName}>
-            <section className="modal-main">
-                <p><i>Name: <strong>{name}</strong></i></p>
-                <p><i>Username: <strong>{username}</strong></i></p>
-                <p><i>Email: <strong>{email}</strong></i></p>
-                <p><i>Phone: <strong>{phone}</strong></i></p>
-                <p><i>Website: <strong>{website}</strong></i></p>
-                <p><i>City: <strong>{addressCity}</strong></i></p>
-                <p><i>Company: <strong>{companyName}</strong></i></p>
-                <p><i>Motto: <strong>{catchPhrase}</strong></i></p>
-                <p><i>Zip Code: <strong>{zipcode}</strong></i></p>
-                <button className="close" onClick={handleClose}>
-                    Close &times;
-                </button>
-            </section>
-        </div>
-    )
+    toggle = nr => () => {
+        let modalNumber = 'modal' + nr;
+        this.setState({
+            [modalNumber]: !this.state[modalNumber]
+        });
+    }
+
+    render(props) {
+        const { handleOpen, handleClose} = this.props;
+        let robotInfo = this.props.showRobot || {};
+        // this.toggle(14);
+        const {
+            id, name, username, email, website, phone,
+            address: { city: addressCity } = { addressCity: 'No city available' },
+            address: { zipcode } = { zipcode: 'No zipcode available' },
+            company: { catchPhrase } = { catchPhrase: 'No catch phrase available' },
+            company: { name: companyName } = { companyName: 'No company name available' },
+        } = robotInfo;
+        return isEmpty(robotInfo) ? null : (
+            <MDBContainer>
+                <MDBBtn color="primary" onClick={this.toggle(14)}>Open Modal</MDBBtn>
+                <MDBModal isOpen={this.state.modal14} toggle={this.toggle(14)} centered>
+                    <MDBModalHeader toggle={this.toggle(14)}>
+                        <img alt='robot' src={`https://robohash.org//${id}?200x200`}/>
+                        <p>{name}</p>
+                    </MDBModalHeader>
+                    <MDBModalBody>
+                        <p><i>Username:</i> <strong>{username}</strong></p>
+                        <p><i>Email:</i> <strong>{email}</strong></p>
+                        <p><i>Phone:</i> <strong>{phone}</strong></p>
+                        <p><i>Website:</i> <strong>{website}</strong></p>
+                        <p><i>City:</i> <strong>{addressCity}</strong></p>
+                        <p><i>Company:</i> <strong>{companyName}</strong></p>
+                        <p><i>Motto:</i> <strong>{catchPhrase}</strong></p>
+                        <p><i>Zip Code:</i> <strong>{zipcode}</strong></p>
+                    </MDBModalBody>
+                    <MDBModalFooter>
+                        <MDBBtn color="secondary" onClick={this.toggle(14)}>Close</MDBBtn>
+                    </MDBModalFooter>
+                </MDBModal>
+            </MDBContainer>
+        );
+    }
 }
 
 export default Modal;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,68 +1,81 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import Modal from '../components/Modal';
 import './App.css';
+import { setSearchField, requestRobots } from '../actions';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchField: '',
-            showModal: false,
-            robotID: -1
-        }
-    }
-
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ 'robots': users }));
-    }
-
-    onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value });
-    }
-
-    onToggleModal = (event) => {
-        this.onShowModal();
-        this.setState({
-            robotID: event.currentTarget.id
-        });
-    }
-
-    onShowModal = () => {
-        this.setState({ showModal: true });
-    };
-    
-    onHideModal = () => {
-        this.setState({ showModal: false });
-    };
-
-    render() {
-    	const {robots, searchField, robotID} = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        });
-        let robotToToggle = robots[robotID-1];
-        return !robots.length ?
-            <h1 className='tc'>Loading</h1> :
-            (
-            	<div className='tc'>
-					<h1 className='f1'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
-					<Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filteredRobots} toggleModal={this.onToggleModal}/>
-                            <Modal show={this.state.showModal} showRobot={robotToToggle} handleClose={this.onHideModal}/>
-                        </ErrorBoundary>
-					</Scroll>
-				</div>
-			)
-    }
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showModal: false,
+      robotID: -1
+    }
+  }
+
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
+
+  onToggleModal = (event) => {
+    this.onShowModal();
+    this.setState({
+      robotID: event.currentTarget.id
+    });
+  }
+
+  onShowModal = () => {
+    this.setState({ showModal: true });
+  };
+
+  onHideModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    console.log(this.state);
+    const { robotID, showModal } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    let robotToToggle = robots[robotID - 1];
+    console.log(showModal);
+    return isPending ?
+      <h1 className='tc'>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={onSearchChange} />
+          <Scroll>
+            <ErrorBoundary>
+              <Modal showRobot={robotToToggle} handleOpen={showModal} handleClose={this.onHideModal} />
+              <CardList robots={filteredRobots} toggleModal={this.onToggleModal} />
+            </ErrorBoundary>
+          </Scroll>
+        </div>
+      )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
